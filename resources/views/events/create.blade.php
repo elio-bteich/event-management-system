a8db892dd94bd6a271fea43046e9329c4e46e2b5
elio-bteich <eliobteich115@gmail.com>
2022-06-07 22:24:31 +0200

    new update

---
+++ resources/views/events/create.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <style></style>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h4>Events
+                            <a href="{{ url('add-event') }}" class="btn btn-primary float-end">Add event</a>
+                        </h4>
+                    </div>
+                    <div class="card-body">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
